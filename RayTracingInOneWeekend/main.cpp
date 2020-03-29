3a4aabcf56cc18dbe770441d14c2a1872ff5fe60
pk <pkukreti@gmail.com>
2020-03-29 13:25:23 +1100

    initial commit

---
+++ RayTracingInOneWeekend/main.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+	const int imageWidth = 200;
+	const int imageHeight = 100;
+
+	printf("P3\n%d %d\n255\n", imageWidth, imageHeight);
+	
+	for (int j = imageHeight - 1; j >= 0; --j) {
+		for (int i = 0; i < imageWidth; ++i) {
+			const double r = static_cast<double>(i) / imageWidth;
+			const double g = static_cast<double>(j) / imageHeight;
+			const double b = 0.2;
+			const int ir = static_cast<int>(255.999 * r);
+			const int ig = static_cast<int>(255.999 * g);
+			const int ib = static_cast<int>(255.999 * b);
+			printf("%d %d %d\n", ir, ig, ib);
+		}
+	}
+
+	return 0;
+}
